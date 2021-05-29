50de9f09740078f6f53042052e53c39132ff3d57
Herbert F. Silva <hbtfsilva@protonmail.com>
2021-05-29 00:54:56 -0300

    Duplicidade

--- ex003.py
+++
@@ -1,9 +0,0 @@
-n1 = int(input('Digite um valor: '))
-n2 = int(input('Digite outro valor: '))
-
-#Maneira antiga de escrever o código, não é errado mais existe uma melhor forma.
-#print('A soma dos valores é', n1+n2)
-
-s = n1 + n2
-print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
-
