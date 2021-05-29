7ecb77f4c7367a976b99b94a19d34af5723edf2c
Herbert F. Silva <hbtfsilva@protonmail.com>
2021-05-29 00:57:16 -0300

    Exercícios práticos

---
+++ ex004.py
@@ -0,0 +1,27 @@
+a = input('Digite algo: ')
+print('O tipo de arquivo primitivo é ', type(a))
+
+#verificar se a string tem espaços;
+print('Só tem espaço? ',a.isspace())
+
+#verificar se a string é númerico;
+print('É um número? ',a.isnumeric())
+
+#verificar se a string é alfabetico;
+print('É alfabetico?', a.isalpha())
+
+#verificar se a string é alfanumerico;
+print('É letra e número? ', a.isalnum())
+
+#verificar se a string é maiúscula;
+print('Está em maiuscula? ', a.isupper())
+
+#verificar se a string é minúscula;
+print('Está em minuscula? ', a.islower())
+
+#verificar se a string é capitalizado (maiúscula e minúscula);
+print('Está capitalizada? ', a.istitle())
+
+
+
+
